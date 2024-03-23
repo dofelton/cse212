48005960ba4519375c6877d302e3f500f98ba7f2
dofelton <dofelton@gmail.com>
2024-03-23 15:27:24 -0500

    w4

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -28,14 +28,21 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void InsertTail(int value) {
         // TODO Problem 1
+         Node newNode = new Node(value);
+        // if list is empty
+        if (_head is null) {
+            _head = newNode;
+            _tail = newNode;
+        }
         // create a new node
         // set previous of new node to tail
         // set next of current tail to new node
         // reassign tail to new node
-        Node newNode = new Node(value);
+        else {
         newNode.Prev = _tail;
         _tail.Next = newNode;
         _tail = newNode;
+        }
     }
 
 
@@ -64,10 +71,15 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void RemoveTail() {
         // TODO Problem 2
+        if (_head == _tail) {
+            _head = null;
+            _tail = null;
+        }
         // reassign the next of the second to last node to null
         // reassign the value of tail to the second to last node
-        _tail.Prev.Next = null;
+        else {_tail.Prev.Next = null;
         _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -108,11 +120,21 @@ public class LinkedList : IEnumerable<int> {
         // TODO Problem 3
         // check if value is head or tail, if so call RemoveHead or RemoveTail
         // reassign the Next to the node after current, reassign Prev to the node before current
-        var curr = _head;
+        Node? curr = _head;
         while (curr is not null) {
             if (curr.Data == value) {
-                curr.Next.Prev = curr.Prev;
-                curr.Prev.Next = curr.Next;
+                if (curr == _head) {
+                    RemoveHead();
+                    return;
+                }
+                else if (curr == _tail) {
+                    RemoveTail();
+                    return;
+                }
+                else {
+                    curr.Next!.Prev = curr.Prev;
+                    curr.Prev!.Next = curr.Next;
+            }
             }
         }
     }
