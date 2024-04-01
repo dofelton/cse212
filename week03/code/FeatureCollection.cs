f90b400f55403210c93295f3d12c97f2fd716a1a
dofelton <dofelton@gmail.com>
2024-04-01 09:16:43 -0500

    4/1

--- week03/code/FeatureCollection.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.VisualBasic;
-
-public class FeatureCollection {
-    // Todo Earthquake Problem - ADD YOUR CODE HERE
-    public string Place { get; set; }
-    public double Mag { get; set; }
-
-}
-    // Create additional classes as necessary
